694533b71a62c912f971db674605fca8d2ccd07e
tourervit <tourervit@gmail.com>
2019-07-07 18:46:05 +0700

    404 page updates

--- src/scenes/404/index.js
+++ src/scenes/404/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Result, Button } from 'antd';
 import history from '@services/history';
 import image from './image';
+import './style.less';
 
 export const NotFound = () => (
   <Result
-    style={{ height: '100vh', background: '#F0F4F3' }}
+    className="not-found"
     title="404"
     subTitle="Sorry, the page you visited does not exist."
     icon={image()}
