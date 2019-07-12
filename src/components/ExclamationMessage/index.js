d875fc1053fc35205c1753676aa20bf79dee83da
tourervit <tourervit@gmail.com>
2019-07-12 19:08:31 +0700

    Message with Exclamation Mark component

---
+++ src/components/ExclamationMessage/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './style.less';
+import { Icon } from 'antd';
+
+export const ExclamationMessage = ({ children }) => (
+  <p className="exclamation__note">
+    <Icon type="exclamation-circle" theme="filled" className="exclamation__icon" />
+    <p>{children}</p>
+  </p>
+);
