d86386d393fc11f01c7184b122e7481d9d0816d2
tourervit <tourervit@gmail.com>
2019-07-30 10:17:17 +0700

    Added versions

--- src/scenes/404/index.js
+++ src/scenes/404/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Result, Button } from 'antd';
 import history from '@services/history';
+import superaxios from '@services/superaxios';
 import image from './image';
 import './style.less';
 
-export const NotFound = () => (
-  <Result
-    className="not-found"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    icon={image()}
-    extra={
-      <Button type="primary" onClick={() => history.push('/')}>
-        Back Home
-      </Button>
-    }
-  />
-);
+export const NotFound = () => {
+  const [version, setVersion] = useState('');
+  const getVersion = async () => {
+    const { data } = await superaxios.get('/version');
+    setVersion(() => data.data);
+  };
+  useEffect(() => {
+    getVersion();
+  }, []);
+
+  return (
+    <>
+      <Result
+        className="not-found"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        icon={image()}
+        extra={
+          <Button type="primary" onClick={() => history.push('/')}>
+            Back Home
+          </Button>
+        }
+      />
+      <div style={{ textAlign: 'center', position: 'relative' }}>
+        <p style={{ marginBottom: 0, fontSize: 10 }}>Front-End 0.0.1</p>
+        <p style={{ marginBottom: 0, fontSize: 10 }}>Back-End {version}</p>
+      </div>
+    </>
+  );
+};
