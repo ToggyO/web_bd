4089b84be9d000a22b484e7849eb225fb834d6ba
tourervit <tourervit@gmail.com>
2019-08-01 19:44:54 +0700

    v.0.0.3

--- src/scenes/404/index.js
+++ src/scenes/404/index.js
@@ -29,7 +29,7 @@ export const NotFound = () => {
         }
       />
       <div style={{ textAlign: 'center', position: 'relative' }}>
-        <p style={{ marginBottom: 0, fontSize: 10 }}>Front-End 0.0.1</p>
+        <p style={{ marginBottom: 0, fontSize: 10 }}>Front-End 0.0.3</p>
         <p style={{ marginBottom: 0, fontSize: 10 }}>Back-End {version}</p>
       </div>
     </>
