f5e45981cf905b654bbfe2add2020fa122e159f0
tourervit <tourervit@gmail.com>
2019-08-27 10:40:08 +0700

    Responsive stage 3

--- src/scenes/HomePage/_components/QuickFiltersTabs/QuickFiltersTabsDisplay.jsx
+++ src/scenes/HomePage/_components/QuickFiltersTabs/QuickFiltersTabsDisplay.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Collapse, Icon } from 'antd';
 import './style.less';
 import { QuickFilterFormContainer } from '@scenes/_components/QuickFilterForm';
 import { getInitialValuesBasedOnNavigatorLanguage } from '@services/navigator';
 
 const { TabPane } = Tabs;
+const { Panel } = Collapse;
 
 const initialState = {
   payment: 'PayPal',
@@ -13,7 +14,22 @@ const initialState = {
 
 const QuickFiltersTabs = () => (
   <div className="quick-filters">
-    <Tabs defaultActiveKey="1" className="bd-tabs quick-filters__tabs">
+    <Collapse
+      className="collapse"
+      expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? -180 : 0} />}
+    >
+      <Panel header={<span className="collapse__header">Show filters</span>}>
+        <Tabs defaultActiveKey="1" className="bd-tabs quick-filters__tabs">
+          <TabPane tab="Quick buy" key="1">
+            <QuickFilterFormContainer type="buy" initialState={initialState} />
+          </TabPane>
+          <TabPane tab="Quick sell" key="2">
+            <QuickFilterFormContainer type="sell" initialState={initialState} />
+          </TabPane>
+        </Tabs>
+      </Panel>
+    </Collapse>
+    <Tabs defaultActiveKey="1" className="bd-tabs quick-filters__tabs uncollapsed">
       <TabPane tab="Quick buy" key="1">
         <QuickFilterFormContainer type="buy" initialState={initialState} />
       </TabPane>
