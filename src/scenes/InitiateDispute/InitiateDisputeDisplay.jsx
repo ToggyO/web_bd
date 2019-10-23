cc6b953b6abf8cd7da09e077ccbd61a7fb48c565
Vitaly Ponomarev <ponomarev@magora-systems.com>
2019-10-23 14:57:58 +0700

    [*] BD-311, BD-312 Initiating dispute

--- src/scenes/InitiateDispute/InitiateDisputeDisplay.jsx
+++ src/scenes/InitiateDispute/InitiateDisputeDisplay.jsx
@@ -1,33 +1,80 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
-import { Form, Input, Row, Col, Button, Upload, Icon } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Form, Input, Row, Col, Button, Upload, Icon, message } from 'antd';
 
 import { AppWrapperContainer } from '@scenes/_components/AppWrapper';
+import superaxios from '@services/superaxios';
 import * as validations from '@services/validations';
 import { ROUTES } from '@config/constants';
+import { getFromLocalState } from '@services/ls';
+
+import { LOCAL_STORAGE_KEYS } from '@config';
+
+import { parseBase64 } from '@utils';
 import './style.less';
 
 const InitiateDisputeDisplay = ({ form, history, location }) => {
-  useEffect(() => {
-    if (!location.state) history.push(ROUTES.HOME);
-  }, []);
+  const [fileList, setFileList] = useState([]);
+  const [uploading, setUploading] = useState(false);
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const props = {
+    onRemove: file => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+
+    beforeUpload: file => {
+      const isLt2M = file.size / 1024 / 1024 < 5;
+      if (!isLt2M) {
+        message.error('File should be smaller than 5Mb');
+        return false;
+      }
+      setFileList([file]);
+      return false;
+    },
+    multiple: false,
+    fileList,
+    accept: 'image/bmp, image/jpeg, image/jpg, image/png, application/pdf',
+  };
+
+  const handleUpload = e => {
     form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        const { _message } = values;
+        const reader = new FileReader();
+        reader.readAsDataURL(fileList[0]);
+        reader.onload = e => {
+          setUploading(true);
+
+          const data = parseBase64(e.target.result);
+
+          superaxios({
+            method: 'post',
+            url: `/trade/${location.state.id}/disputed`,
+            data: { message: _message, ...data },
+            config: { headers: { 'Content-Type': 'text/plain' } },
+          })
+            .then(() => {
+              setFileList([]);
+              setUploading(false);
+
+              history.replace(ROUTES.SETTINGS.ROOT);
+              message.success('Dispute has been initiated');
+            })
+            .catch(() => {
+              setUploading(false);
+              message.error('Upload has been failed.');
+            });
+        };
       }
     });
   };
 
-  const normFile = e => {
-    console.log('Upload event:', e);
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e && e.fileList;
-  };
+  useEffect(() => {
+    if (!location.state) history.push(ROUTES.HOME);
+  }, []);
 
   return (
     <AppWrapperContainer>
@@ -35,33 +82,40 @@ const InitiateDisputeDisplay = ({ form, history, location }) => {
         <div className="initiate-dispute">
           <h1>Initiate a dispute</h1>
           <Row>
-            <Col md={12}>
-              <Form onSubmit={handleSubmit} hideRequiredMark>
-                <Form.Item label="Enter contact email address">
+            <Col lg={12}>
+              <Form hideRequiredMark>
+                <Form.Item label="Email address">
                   {form.getFieldDecorator('email', {
+                    initialValue: getFromLocalState(LOCAL_STORAGE_KEYS.USER).email,
                     rules: validations.email,
-                  })(<Input type="email" placeholder="riley411@gmail.com" />)}
+                  })(<Input type="email" placeholder="riley411@gmail.com" disabled />)}
                 </Form.Item>
                 <Form.Item label="Enter message">
-                  {form.getFieldDecorator('message', {
+                  {form.getFieldDecorator('_message', {
                     initialValue: location.state ? location.state.text : '',
-                  })(<Input.TextArea rows={5} placeholder="riley411@gmail.com" />)}
+                  })(<Input.TextArea rows={5} placeholder="Describe your problem here" />)}
                 </Form.Item>
-                <Form.Item extra="Max size 5 Mb, only .jpeg, .jpg, .png, .pdf">
-                  {form.getFieldDecorator('upload', {
-                    valuePropName: 'fileList',
-                    getValueFromEvent: normFile,
-                  })(
-                    <Upload name="logo" accept="image/jpeg, image/jpg, image/png, .pdf">
-                      <Button style={{ height: 32 }}>
-                        <Icon type="upload" /> Upload
-                      </Button>
-                    </Upload>
+                <div style={{ marginBottom: 16 }}>
+                  <Upload {...props}>
+                    <Button style={{ height: 32 }}>
+                      <Icon type="upload" /> Upload
+                    </Button>
+                  </Upload>
+                  {fileList.length === 0 && (
+                    <p style={{ color: 'rgba(0,0,0,0.45)', margin: '8px 0 16px' }}>
+                      Max size 5 Mb, only .jpeg, .jpg, .png, .pdf
+                    </p>
                   )}
-                </Form.Item>
+                </div>
                 <Row gutter={8}>
                   <Col lg={7}>
-                    <Button type="primary" htmlType="submit" style={{ width: '100%', marginBottom: 12 }}>
+                    <Button
+                      loading={uploading}
+                      type="primary"
+                      htmlType="submit"
+                      style={{ width: '100%', marginBottom: 12 }}
+                      onClick={handleUpload}
+                    >
                       Send apply
                     </Button>
                   </Col>
